fa7aac67c6895daca7cdc3576f5ffad7db7386f1
Jamniczek <misiak.mac@gmail.com>
2018-03-07 20:45:12 +0100

    refactored and added auth

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const passportConfig = require('./services/passport');
+const authRoutes = require('./routes/authRoutes')
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-const PORT = process.env.PORT || 3000
+authRoutes(app);
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
 app.listen(PORT, () => {
     console.log('App running on port: 3000');
 });
