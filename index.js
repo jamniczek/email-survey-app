cb99b2731239bb1e1c730b8bcc45f09e75070e65
Jamniczek <misiak.mac@gmail.com>
2018-03-19 17:50:07 +0100

    created prod and dev env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/user');
 // const user = require('./models/user');
@@ -12,6 +14,15 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
 
 authRoutes(app);
 
